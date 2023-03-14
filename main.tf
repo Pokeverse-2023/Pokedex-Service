7b3083ae88ea22194c300e78405806364bb86da7
Manjunath PV <manjunathpv@outlook.com>
2023-03-14 09:15:16 +0530

    Changing version to 3.10

--- main.tf
+++ main.tf
@@ -1,4 +1,25 @@
-module "Pokedex-Service" {
-  source = "git::https://github.com/pokeverse-2023/terraform-hub.git//services"
+terraform {
+  backend "s3" {
+    bucket = "pokeverse"
+    key    = "terraform/all-state/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
 
+module "Pokedex-Service" {
+  source               = "git::https://github.com/pokeverse-2023/terraform-hub.git//services"
+  function_name        = var.function_name
+  function_runtime     = var.function_runtime
+  handler              = var.handler
+  memory               = var.memory
+  default_tags         = var.default_tags
+  mongodb_cluster_name = var.mongodb_cluster_name
+  mongodb_private_key  = var.mongodb_private_key
+  mongodb_project_name = var.mongodb_project_name
+  mongodb_public_key   = var.mongodb_public_key
+  mongodb_username     = var.mongodb_username
+  source_code_path     = var.source_code_path
+  timeout              = var.timeout
+  output_path          = var.output_path
+  region               = var.region
 }
---
+++ variables.tf
@@ -0,0 +1,60 @@
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "default_tags" {
+  type = object({
+    Terraform    = bool
+    Service      = string
+    Organization = string
+  })
+}
+
+variable "function_name" {
+  type = string
+}
+
+variable "function_runtime" {
+  type = string
+}
+
+variable "handler" {
+  type = string
+}
+
+variable "memory" {
+  type = number
+}
+
+variable "timeout" {
+  type = number
+}
+
+variable "source_code_path" {
+  type = string
+}
+
+variable "output_path" {
+  type = string
+}
+
+variable "mongodb_public_key" {
+  type = string
+}
+
+variable "mongodb_private_key" {
+  type = string
+}
+
+variable "mongodb_project_name"{
+  type = string
+}
+
+variable "mongodb_cluster_name" {
+  type = string
+}
+
+variable "mongodb_username"{
+  type = string
+}
